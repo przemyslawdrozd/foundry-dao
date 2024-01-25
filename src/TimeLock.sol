a3e4e5907ce8897b8e53dbd55edfc7d5d29f1d41
Przemo <przemo06133@gmail.com>
2024-01-25 19:09:34 +0100

    Add unit test for governor update

--- src/TimeLock.sol
+++ src/TimeLock.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.0;
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract TimeLock is TimelockController {
+    bytes32 public constant TIMELOCK_ADMIN_ROLE = keccak256("TIMELOCK_ADMIN_ROLE");
+
     // minDelay is how long you have to wait before executing
     // proposers is the list of addresses that can propose
     // executors is the list of addresses that can execute
--- src/govToken.sol
+++ src/govToken.sol
@@ -16,4 +16,8 @@ contract GovToken is ERC20, ERC20Permit, ERC20Votes {
     function nonces(address owner) public view override(ERC20Permit, Nonces) returns (uint256) {
         return super.nonces(owner);
     }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
 }
